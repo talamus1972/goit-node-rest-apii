31046c2147b440cbbdf1606c4ad650ab07cde134
talamus1972 <talamus1972@gmail.com>
2024-05-24 19:55:02 +0300

    qqqq

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,12 +1,13 @@
-import sgMail from "@sendgrid/mail"
+import sgMail from "@sendgrid/mail";
 import "dotenv/config";
 
 const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+const FROM_EMAIL = process.env.FROM_EMAIL;
 
-sgMail.setApiKey(SENDGRID_API_KEY)
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 export const sendEmail = async (data) => {
-    const email = { ...data, from: "talamus1972@gmail.com" }
-    await sgMail.send(email)
-    return true
-}
+  const email = { ...data, from: FROM_EMAIL };
+  await sgMail.send(email);
+  return true;
+};
